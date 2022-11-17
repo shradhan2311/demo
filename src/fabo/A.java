91c7b3afc2772c0b944109e121e44a11c505d18d
admin <admin@SHREE.domain.name>
2022-11-17 16:42:51 +0530

    fg

---
+++ src/fabo/A.java
@@ -0,0 +1,33 @@
+package fabo;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		int num, a=0,b=0, c=1;
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the no ");
+		
+		num=sc.nextInt();
+		
+		System.out.println("fabolanci Series");
+		
+		for(int i=0 ;i<=num; i++)
+		{
+			a=b;
+			b=c;
+			c=a+b;
+			
+			System.out.println(a+ " " );
+			
+		}
+		
+		
+	}
+
+}
